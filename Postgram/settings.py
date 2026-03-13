3232f4fca97c432b5cbcf2da382b22577e2aa1c4
sakhawat ali <msakhawatali131@gmail.com>
2026-03-13 11:09:40 +0500

    apply postgresql database

--- Postgram/settings.py
+++ Postgram/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'Postgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Postgram',
+        'USER' : 'postgres',
+        'PASSWORD' : 'sakhawat',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
